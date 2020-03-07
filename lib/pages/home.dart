167bec984d080623c22ee8692f5d80fa3920e6b5
Dung Tri Nguyen <ntdunglc@gmail.com>
2020-03-07 16:15:44 -0500

    add flutter_map

---
+++ lib/pages/home.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
+import '../widgets/drawer.dart';
+
+class HomePage extends StatelessWidget {
+  static const String route = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    var markers = <Marker>[
+      Marker(
+        width: 80.0,
+        height: 80.0,
+        point: LatLng(51.5, -0.09),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            colors: Colors.blue,
+            key: ObjectKey(Colors.blue),
+          ),
+        ),
+      ),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Convid-19 Tracker')),
+      drawer: buildDrawer(context, route),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Flexible(
+              child: FlutterMap(
+                options: MapOptions(
+                  center: LatLng(51.5, -0.09),
+                  zoom: 5.0,
+                ),
+                layers: [
+                  TileLayerOptions(
+                    urlTemplate:
+                        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    subdomains: ['a', 'b', 'c'],
+                    // For example purposes. It is recommended to use
+                    // TileProvider with a caching and retry strategy, like
+                    // NetworkTileProvider or CachedNetworkTileProvider
+                    tileProvider: NonCachingNetworkTileProvider(),
+                  ),
+                  MarkerLayerOptions(markers: markers)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../pages/home.dart';
+import '../pages/my_health.dart';
+
+Drawer buildDrawer(BuildContext context, String currentRoute) {
+  return Drawer(
+    child: ListView(
+      children: <Widget>[
+        const DrawerHeader(
+          child: Center(
+            child: Text('Convid-19 Tracker'),
+          ),
+        ),
+        ListTile(
+          title: const Text('Home'),
+          selected: currentRoute == HomePage.route,
+          onTap: () {
+            Navigator.pushReplacementNamed(context, HomePage.route);
+          },
+        ),
+        ListTile(
+          title: const Text('My Health'),
+          selected: currentRoute == MyHealthPage.route,
+          onTap: () {
+            Navigator.pushReplacementNamed(context, MyHealthPage.route);
+          },
+        ),      ],
+    ),
+  );
+}
